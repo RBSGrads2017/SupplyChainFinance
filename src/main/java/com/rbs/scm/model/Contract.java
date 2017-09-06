209203f2217c5805762d1352459c4f51b7ae3a8d
sankeerth <sankeerth@gmail.com>
2017-09-06 15:48:21 +0530

    added invoice project to the local branch

---
+++ src/main/java/com/rbs/scm/model/Contract.java
@@ -0,0 +1,26 @@
+package com.rbs.scm.model;
+
+public class Contract {
+private int contract_number;
+private int sellerID;
+private int buyerID;
+public int getContract_number() {
+	return contract_number;
+}
+public void setContract_number(int contract_number) {
+	this.contract_number = contract_number;
+}
+public int getSellerID() {
+	return sellerID;
+}
+public void setSellerID(int sellerID) {
+	this.sellerID = sellerID;
+}
+public int getBuyerID() {
+	return buyerID;
+}
+public void setBuyerID(int buyerID) {
+	this.buyerID = buyerID;
+}
+
+}
---
+++ src/main/java/com/rbs/scm/util/CustomMessage.java
@@ -0,0 +1,13 @@
+package com.rbs.scm.util;
+
+public class CustomMessage {
+	 private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
