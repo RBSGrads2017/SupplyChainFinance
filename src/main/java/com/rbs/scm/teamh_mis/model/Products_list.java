d6686d99a22461ed0d0aca4c3751bb70571de3ff
divyaparvathe95 <divya.parvathe95@gmail.com>
2017-09-07 15:46:51 +0530

    Manual Merge of TeamH_MIS branch

---
+++ src/main/java/com/rbs/scm/teamh_mis/model/Products_list.java
@@ -0,0 +1,26 @@
+package com.rbs.scm.teamh_mis.model;
+
+public class Products_list {
+	private int product_id;
+	 private String category;
+	 private String name;
+ public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
